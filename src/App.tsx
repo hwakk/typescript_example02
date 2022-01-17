82ceaf6fb17db63c9572738b0c13b4b5a8e712dd
hwakk <napia1014@gmail.com>
2022-01-18 01:06:58 +0900

    포켓몬 찾기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootReducerType } from './Store';
+import { fetchPokemonData } from './actions/PokemonActions';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const pokemonReducer = useSelector((state:RootReducerType) => state.PokemonReducer);
+  const dispatch = useDispatch();
+
+  const handlePokemonName = (event: React.ChangeEvent<HTMLInputElement>) => setPokemonName(event.target.value);
+  const searchButtonTapped = () => {
+    dispatch(fetchPokemonData(pokemonName));
+  }
+
   return (
     <div className="App">
-      
+      <input value={pokemonName} onChange={handlePokemonName} />
+      <button onClick={searchButtonTapped}>포켓몬찾기</button>
+      <div>
+        {pokemonReducer.success && <div>
+          <p>{pokemonName}</p>
+          {pokemonReducer.pokemon?.abilities.map((ability) => {
+            return <div><p>{ability.ability.name}</p>
+            <p>{ability.slot}</p></div>
+          })}
+          <img src={pokemonReducer.pokemon?.sprites.front_default} />
+        </div>}
+      </div>
     </div>
   );
 }
--- src/reducers/PokemonReducer.ts
+++ src/reducers/PokemonReducer.ts
@@ -1,15 +1,33 @@
+import { PokemonDispatchType, PokemonType, POKEMON_FAIL, POKEMON_SUCCESS, } from "../actions/PokemonActionTypes";
+
 interface InitialState {
-
+  success: boolean,
+  pokemon?: PokemonType
 }
 
-const initialState = {
-
+const initialState: InitialState = {
+  success: false
 }
 
-const PokemonReducer = (state=initialState, action: any) => {
+const PokemonReducer = (state=initialState, action: PokemonDispatchType): InitialState => {
   switch(action.type) {
+    case POKEMON_FAIL:
+      return {
+        ...state,
+        success: false
+      }
+    case POKEMON_SUCCESS:
+      const {abilities, sprites} = action.payload;
+      return {
+        ...state,
+        success: true,
+        pokemon: {
+          abilities,
+          sprites
+        }
+      }
     default:
-      return true;
+      return state;
   }
 }
 
