5fd7749012e035691c27fe13f7bbc9421058b8fa
hwakk <napia1014@gmail.com>
2022-01-17 23:13:33 +0900

    first commit

---
+++ src/reducers/PokemonReducer.ts
@@ -0,0 +1,16 @@
+interface InitialState {
+
+}
+
+const initialState = {
+
+}
+
+const PokemonReducer = (state=initialState, action: any) => {
+  switch(action.type) {
+    default:
+      return true;
+  }
+}
+
+export default PokemonReducer;
